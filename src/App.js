01780b1c664748d7fa931c8c37ec0ea57b6a13f7
fANZYo <www.iehl@gmail.com>
2018-03-17 15:50:21 +0000

    Set up project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 import './App.css';
+
+// Components
+import Nav from './components/Nav/NavModule';
 
 class App extends Component {
 	render() {
 		return (
-			<h1>hello</h1>
+			<Router>
+				<MuiThemeProvider>
+					<React.Fragment>
+						<Nav />
+						<RaisedButton label="Default" />
+					</React.Fragment>
+				</MuiThemeProvider>
+			</Router>
 		);
 	}
 }
