9a0c2ed7a78b47892ce23ebd09613caea4c932cc
fANZYo <www.iehl@gmail.com>
2018-03-17 17:11:03 +0000

    Add nav with drawer

--- src/components/Nav/NavModule.js
+++ src/components/Nav/NavModule.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import AppBar from 'material-ui/AppBar';
+import { Link } from 'react-router-dom';
+
+const drawerStyle = {
+	paddingTop: "48px",
+};
+
+const closeButton = {
+	position: "absolute",
+	right: 0,
+	top: 0
+};
 
 class Nav extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isOpen: false
+		};
+
+		this.handleClick = this.handleClick.bind(this);
+		this.handleClose = this.handleClose.bind(this);
+	}
+
+	handleClick() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		})
+	}
+
+	handleClose() {
+		this.setState({
+			isOpen: false
+		})
+	}
+
 	render() {
 		return (
-			<p>Nav</p>
+			<React.Fragment>
+				<AppBar
+					title="V.ex"
+					onTitleClick={this.handleClick}
+					onLeftIconButtonClick={this.handleClick}
+				/>
+				<Drawer
+					open={this.state.isOpen}
+					docked={false}
+					containerStyle={drawerStyle}
+				>
+					<IconButton style={closeButton} onClick={this.handleClose}><NavigationClose /></IconButton>
+					<MenuItem onClick={this.handleClose}><Link to="/">Index</Link></MenuItem>
+					<MenuItem onClick={this.handleClose}><Link to="/events">Events</Link></MenuItem>
+					<MenuItem onClick={this.handleClose}><Link to="/profile">Profile</Link></MenuItem>
+				</Drawer>
+			</React.Fragment>
 		);
 	}
 }
