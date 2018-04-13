4be81ea869f1e167aa66e78a0265282dfcd068ff
fANZYo <www.iehl@gmail.com>
2018-04-13 22:22:24 +0100

    Provide catch to json parsin

--- server/app.js
+++ server/app.js
@@ -91,6 +91,7 @@ app.get('/private/:user', (req, res) => {
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
+	console.log(path.resolve(__dirname, '..', 'build', 'index.html'));
 	res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
